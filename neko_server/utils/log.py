d543bcc5de2fad9b214e54e860384c2d1520a5c6
Victor Lai <victor.lai@foxmail.com>
2021-05-25 16:18:55 +0800

    update

--- neko_server/utils/log.py
+++ neko_server/utils/log.py
@@ -3,19 +3,17 @@ import time
 
 
 def log(*args, **kwargs):
-    # time_format = '%Y/%m/%d %H:%M:%S'
-    # localtime = time.localtime(int(time.time()))
-    # formatted = time.strftime(time_format, localtime)
-    # print(formatted, flush=True, *args, **kwargs)
-    pass
+    time_format = '%Y/%m/%d %H:%M:%S'
+    localtime = time.localtime(int(time.time()))
+    formatted = time.strftime(time_format, localtime)
+    print(formatted, flush=True, *args, **kwargs)
 
 
 def log_debug(*args, **kwargs):
-    # time_format = '%Y/%m/%d %H:%M:%S'
-    # localtime = time.localtime(int(time.time()))
-    # formatted = time.strftime(time_format, localtime)
-    # print(formatted, flush=True, *args, **kwargs)
-    pass
+    time_format = '%Y/%m/%d %H:%M:%S'
+    localtime = time.localtime(int(time.time()))
+    formatted = time.strftime(time_format, localtime)
+    print(formatted, flush=True, *args, **kwargs)
 
 
 def log_error(*args, **kwargs):
